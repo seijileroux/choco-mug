ba57d915cbfa7ee11bde69bfffe02faa33926088
Seiji Leroux <45913147+seijileroux@users.noreply.github.com>
2026-01-16 23:50:54 +0300

    Add ProductCardProps interface

--- interfaces.ts
+++ interfaces.ts
@@ -2,3 +2,11 @@ export interface NavItemProps {
   label: string;
   active?: boolean;
 }
+
+export interface ProductCardProps {
+  name: string;
+  description: string;
+  price: string;
+  rating: string;
+  image: string;
+}
